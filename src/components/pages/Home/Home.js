05225237eccff66daf54eaf1fdc714732935a860
Jess Greene <jess.greene9@gmail.com>
2021-08-13 11:04:43 -0400

    updating resume and about me:

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -8,39 +8,34 @@ import "./style.css";
 function About() {
   return (
     <>
-        <header className="hero">
-          <div className="inner">
-            <h1 className="name text-center">Jess Greene</h1>
-          </div>
-          <h3 className="text-center">Coding Boot Camp Student </h3>
-        </header>
+      <header className="hero">
+        <div className="inner">
+          <h1 className="name text-center">Jess Greene</h1>
+        </div>
+        <h3 className="text-center">Coding Boot Camp Student </h3>
+      </header>
       <Container fluid="md" className="home m-3">
-          <p>
-            <img
-              src={picture}
-              alt="Jess Greene"
-              className="bio m-4 pull-right"
-            />
-          </p>
+        <p>
+          <img src={picture} alt="Jess Greene" className="bio m-4 pull-right" />
+        </p>
         <h2 className="text-left">About Me</h2>
         <div className="about-me">
           <br></br>
-          
+
           <p>
-            I currently live in Wilmington, North Carolina. I graduated from the
-            University of North Carolina at Wilmington in 2007 with an English
-            degree and a Teacher's License. Over the years, I have discovered
-            that I am incredibly interested in what goes on behind the scenes
-            of websites and applications. This has led me to enrolling in the
-            Coding Boot Camp program at the University of North Carolina at
-            Charlotte.
+            I am a Full Stack Web Developer based in Wilmington, NC. I received
+            Certification through University of North Carolina at Charlotte
+            Coding Bootcamp. I am a natural problem solver and love challenging
+            myself to continue to learn and improve on my existing skill sets.
+            After several years of teaching myself different computer skills as
+            an Operations Manager, I decided to truly expand my knowledge by
+            enrolling in the immersive experience of the boot camp.
           </p>
           <p>
-            I intend to use the skills that I have obtained in my various roles
-            to assist me in my venture to change my career path into something I
-            truly love and enjoy. My goal is to continue learning after the
-            program has completed and to build on skills learned in this course
-            to create a new and better future for myself.
+            I excel in collaboration with others, as well as working on my own
+            to meet deadlines. My goal is to continue pushing myself and to
+            build on skills learned in this course to create a new and better
+            future for myself.
           </p>
         </div>
         <Footer />
